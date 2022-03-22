67e888da542e12ffdf51d67c2ae26d4b313d98a4
manupruiz <rayerito23@gmail.com>
2022-03-22 17:48:45 +0100

    contraseña cifrada y enviada

--- p1/login.php
+++ p1/login.php
@@ -16,8 +16,31 @@
         </FORM>
 
         <?php
+            $passwd= $_GET['passwd'];
+            $correo= $_GET['correo'];
+            $hash = password_hash($passwd, PASSWORD_DEFAULT, [10]);
+
              $conexion = mysqli_connect("127.0.0.1","root","","bduca");
-             $consulta = mysqli_query($conexion, "SELECT id_usuario, tipo FROM usuario WHERE ");
+             $consulta = mysqli_query($conexion, "SELECT id_usuario, tipo, passwd FROM usuario WHERE correo = $correo");
+            
+            //Si la consulta arroja un resultado (existe el usuario indicado y tiene asociada la contraseña indicada)
+            //abrimos una sesión, que va a enlazar con una página en función del tipo de usuario que sea.
+            //¿Volver atrás y mostrar mensaje de error en el caso de que no?
+
+            $fila = mysqli_fetch_array($consulta);
+
+            if(mysqli_num_rows() == 1 && password_verify($fila["passwd"], $hash)){ //Si solamente hay una coincidencia significa que el usuario existe y ha metido los datos correctamente.
+                              
+                session_start();
+                
+
+
+
+
+            }else{
+
+            }
+            
 
         ?>
     </body>
--- relacion1/ej3.php
+++ relacion1/ej3.php
@@ -5,8 +5,21 @@
     </head>
     <body>
         <?php
-        $passwd='c123456';
+        class Password {
+            public static function hash($password) {
+                return password_hash($password, PASSWORD_DEFAULT, [10]);
+            }
+            public static function verify($password, $hash) {
+                return password_verify($password, $hash);
+            }
+        }
+        $passwd='c1234567890';
         $hash = password_hash($passwd, PASSWORD_DEFAULT, [10]);
+
+        if(password_verify($passwd, $hash)){
+            echo 'true';
+            echo "<br>";
+        }
         echo "$hash";
 
         ?>
